7ff3a726bccd38e6d4a5c12eae0e8ac7ea2e9aa8
Aden913 <coxaden@gmail.com>
2023-04-13 23:58:33 -0400

    fixed 8

--- my-app/public/webpages/php/coxaden8/model/dbUserCheck.php
+++ my-app/public/webpages/php/coxaden8/model/dbUserCheck.php
@@ -12,7 +12,7 @@ function is_valid_admin_login($name, $pass){
     //echo $pass;
     //print_r($_POST);
 
-    $queryUser = 'SELECT account_type FROM users WHERE name = :name AND pass = :pass';
+    $queryUser = 'SELECT account_type FROM users WHERE name = :name AND password = :pass';
     $statement1 = $db->prepare($queryUser);
     $statement1->bindValue(':name', $strName);
     $statement1->bindValue(':pass', $strPass);
